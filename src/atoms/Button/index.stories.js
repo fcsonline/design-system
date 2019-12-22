b07fbbcb873ccea748bf57c8fd78c7cb956bc763
Ferran Basora <fcsonline@gmail.com>
2019-12-22 13:19:01 +0000

    Add documentation

--- src/atoms/Button/index.stories.js
+++ src/atoms/Button/index.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text } from "@storybook/addon-knobs";
 import { withA11y } from '@storybook/addon-a11y';
+import MDX from './index.notes.mdx';
 
 import Center from '../../storybook/Center';
 
@@ -24,4 +25,6 @@ stories.add('Simple', () => (
       label={text('Label', 'Submit')}
     />
   </Center>
-))
+),
+{ docs: { page: MDX } }
+)
